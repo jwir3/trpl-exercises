021fa2be1d03f9fd88800b5410a1cb186b0268cf
Scott Johnson <jaywir3@gmail.com>
2018-09-04 10:41:44 -0500

    Change guess name to a_guess.

--- Chapter2-ProgrammingGuessingGame/guessing_game/src/main.rs
+++ Chapter2-ProgrammingGuessingGame/guessing_game/src/main.rs
@@ -14,7 +14,6 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    // println!("Secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -25,30 +24,30 @@ fn main() {
         // The :: on String indicates this is an _associated function_ of the String class, meaning
         // it is called on the String class, rather than on an instance. Also known as a static
         // method in other languages.
-        let mut guess = String::new();
+        let mut a_guess = String::new();
 
         // The ampersand indicates this is a reference to guess that's being passed to the function.
         // References, like variables, are immutable by default. So, we need to tell it to make the
         // reference mutable with '&mut'.
         io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut a_guess)
         // .read_line returns an io::Result, which is an enum
         // The possible values are Ok or Err. This is to encode failure conditions more succinctly.
         // If the value is Err, then expect() will crash and report the message to the stdout.
-        .expect("Failed to read line");
+        .expect("Failed to read a_guess");
 
         // We need to convert guess to a number in order to perform the comparison later on. Note that
         // the name 'guess' is _shadowed_, allowing us to basically replace it without coming up with
         // another variable name.
-        let guess: u32 = match guess.trim().parse() {
+        let a_guess: u32 = match a_guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", a_guess);
 
         // A match expression consists of arms. Basically, it's a switch statement in other languages.
-        match guess.cmp(&secret_number) {
+        match a_guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
